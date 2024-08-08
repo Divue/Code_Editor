d353f7cdd5d0e7799d3709f2804101a81da1eb3a
Divue <divue2003@gmail.com>
2024-08-09 00:27:51 +0530

    make a running code editor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import './App.css'
-import FetchData from './components/FetchData'
+import "./App.css";
+import FetchData from "./components/FetchData";
+import CodeEditor from "./components/CodeEditor";
+import { Box } from "@chakra-ui/react";
 
 function App() {
-
   return (
-   <>
-    <div>
-      <h1>Welcome</h1>
-      <FetchData/>
-    </div>
-   </>
-  )
+    <>
+      <div>
+        <Box minH="100vh" bg="*0f0a19" color="gray.500" px={6} py={8}>
+        <h1>Welcome</h1>
+                <FetchData/>
+
+          <CodeEditor/>
+        </Box>
+        
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/CodeEditor.jsx
+++ frontend/src/components/CodeEditor.jsx
@@ -1,9 +1,52 @@
-const CodeEditor =()=>{
-    return(
-        <Box>
+import React, { useRef, useState } from "react";
+import MonacoEditor from "react-monaco-editor";
+import { Box, HStack } from "@chakra-ui/react";
+import Editor from "@monaco-editor/react";
+import LanguageSelector from "./LanguageSelector";
+import { CODE_SNIPPETS } from "../constants";
+import Output from "./Output";
+//import { languages } from "monaco-editor";
 
+const CodeEditor = () => {
+  const editorRef = useRef();
+  const [value, setValue] = useState("");
+  const [language, setLanguage] = useState("javascript");
+
+  const onMount = (editor) => {
+    editorRef.current = editor;
+    editor.focus();
+  };
+
+  const onSelect = (language) => {
+    setLanguage(language);
+    setValue(CODE_SNIPPETS[language]);
+    // console.log(value);
+  };
+
+  return (
+    <Box>
+      <HStack spacing={4}>
+        <Box w="50%">
+
+          <LanguageSelector
+            language={language}
+            onSelect={onSelect}
+          ></LanguageSelector>
+          <Editor
+            height="75vh"
+            theme="vs-dark"
+            language={language}
+            defaultValue={CODE_SNIPPETS[language]}
+            onMount={onMount}
+            value={value}
+            onChange={(value) => setValue(value)}
+          />
+          ;
         </Box>
-    )
-}
+        <Output editorRef={editorRef} language={language}/>
+      </HStack>
+    </Box>
+  );
+};
 
 export default CodeEditor;
